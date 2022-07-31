8f0c1d1317fc4ad8d962399256fe1c0bc96f90ba
MrunaliDeore <mrunalideoreohk1@gmail.com>
2022-07-31 18:12:27 +0530

    Added : Main class file - message

---
+++ MainClass.java
@@ -0,0 +1,13 @@
+package ExceptionHandling;
+
+public class MainClass {
+	public static void main(String[]args){
+		System.out.println("Assignment No - 21\n");
+		System.out.println("Mood Analyzer Problem\n"
+				+ "Emphasis on\n"
+				+ "- Junit Test Cases\n"
+				+ "- Exception Handling\n"
+				+ "- Custom Exceptions\n"
+				+ "- Reflections");
+	}
+}
